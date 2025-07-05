a686e036d6d714f8b172b8defcd04e79954fb096
Cyzo50218 <myfavoritemappingswar@gmail.com>
2025-07-05 13:24:45 +0800

    update searchresults.js

--- dashboard/searchresults/searchresults.js
+++ dashboard/searchresults/searchresults.js
@@ -484,16 +484,14 @@ const commentIconHTML =
     `<span class="flex items-center text-slate-500" title="${commentCount} comments">
          <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path></svg>
          <span class="text-[10px] ml-1">${commentCount}</span>
-       </span>` :
-    '';
+       </span>`;
 
 const likeCount = task.likedAmount || 0;
 const likeIconHTML =
     `<span class="flex items-center text-slate-500" title="${likeCount} likes">
          <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="text-red-500 fill-current"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>
          <span class="text-[10px] ml-1">${likeCount}</span>
-       </span>` :
-    '';
+       </span>`;
 
 
 // Set the final HTML for the cell
