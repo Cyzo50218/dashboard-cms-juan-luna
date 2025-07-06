6deaef29a7ace511a008ae7673eb4fc3ebebbe46
Cyzo50218 <myfavoritemappingswar@gmail.com>
2025-07-06 10:31:19 +0800

    update header.js

--- dashboard/header/header.js
+++ dashboard/header/header.js
@@ -660,6 +660,49 @@ function enterSearchResults() {
   return containerDiv;
 }
 
+function hslToRgb(h, s, l) {
+  s /= 100;
+  l /= 100;
+  
+  let c = (1 - Math.abs(2 * l - 1)) * s,
+    x = c * (1 - Math.abs((h / 60) % 2 - 1)),
+    m = l - c / 2,
+    r = 0,
+    g = 0,
+    b = 0;
+  
+  if (0 <= h && h < 60) {
+    r = c;
+    g = x;
+    b = 0;
+  } else if (60 <= h && h < 120) {
+    r = x;
+    g = c;
+    b = 0;
+  } else if (120 <= h && h < 180) {
+    r = 0;
+    g = c;
+    b = x;
+  } else if (180 <= h && h < 240) {
+    r = 0;
+    g = x;
+    b = c;
+  } else if (240 <= h && h < 300) {
+    r = x;
+    g = 0;
+    b = c;
+  } else if (300 <= h && h < 360) {
+    r = c;
+    g = 0;
+    b = x;
+  }
+  r = Math.round((r + m) * 255);
+  g = Math.round((g + m) * 255);
+  b = Math.round((b + m) * 255);
+  
+  return [r, g, b];
+}
+
 function hslToHex(h, s, l) {
   const [r, g, b] = hslToRgb(h, s, l);
   const toHex = (c) => {
@@ -668,7 +711,6 @@ function hslToHex(h, s, l) {
   };
   return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
 }
-
 
 async function displaySearchResults(tasks, projects, people, messages) {
   const halfQueryDiv = document.getElementById('half-query');
@@ -1754,7 +1796,7 @@ onAuthStateChanged(auth, async (user) => {
         const projects = results[0]?.hits || [];
         const tasks = results[1]?.hits || [];
         console.log("Found Projects:", projects);
-console.log("Found Tasks:", tasks);
+        console.log("Found Tasks:", tasks);
         /*
         const people = results[2]?.hits || [];
         {
