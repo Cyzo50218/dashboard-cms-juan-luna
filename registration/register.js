69c6d890903d163607b1097e40406bf9d19b70a3
Cyzo50218 <myfavoritemappingswar@gmail.com>
2025-07-11 05:54:25 +0800

    fgd

--- registration/register.js
+++ registration/register.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import {
   getAuth,
+  onAuthStateChanged,
   GoogleAuthProvider,
   signInWithPopup,
   createUserWithEmailAndPassword,
