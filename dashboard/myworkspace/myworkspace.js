ccdc7aebd8946227a6aa08ade68ee0069495a4df
Cyzo50218 <myfavoritemappingswar@gmail.com>
2025-07-12 23:48:46 +0800

    DF

--- dashboard/myworkspace/myworkspace.js
+++ dashboard/myworkspace/myworkspace.js
@@ -49,6 +49,8 @@ export function init(params) {
   const staffListContainer = workspaceSection.querySelector("#staff-list");
   const staffCountLink = workspaceSection.querySelector("#staff-count-link");
   const inviteButton = workspaceSection.querySelector("#invite-btn");
+  const inviteButtonMembers = workspaceSection.querySelector("#invite-btn");
+  const invitePlusMembers = workspaceSection.querySelector("#add-staff-btn");
   const createWorkBtn = workspaceSection.querySelector("#create-work-btn");
   
 
